e5000a071c606fab241abc3dac5fe50eeb49e51b
Senuri1276 <hashanmadushanka022@gmail.com>
2025-06-24 23:32:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=stx0CIYB#XNl5J0n5z1959XrNpQ3TMcGKOX1L-5T6hqfK7G7FnVs",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=o9IUGAga#cHqKAZhMr6LAKoaUZbwMbE7Dzn8-qnJmt-YHp6-ifWI",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
